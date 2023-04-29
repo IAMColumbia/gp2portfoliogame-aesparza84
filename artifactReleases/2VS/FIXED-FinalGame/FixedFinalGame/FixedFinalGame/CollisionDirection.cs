41e8ea0b27c2605abebfb5063a885b58610b057f
Angelo Esparza <384290@colum.edu>
2023-04-29 15:30:34 -0500

    more gerneral static collison method

--- artifactReleases/2VS/FIXED-FinalGame/FixedFinalGame/FixedFinalGame/CollisionDirection.cs
+++ artifactReleases/2VS/FIXED-FinalGame/FixedFinalGame/FixedFinalGame/CollisionDirection.cs
@@ -10,28 +10,46 @@ namespace FixedFinalGame
     public static class CollisionDirection
     {
         public static bool IntersectsLeft(this Rectangle c, Rectangle tile)
-        { 
+        {
             return c.Left < tile.Left &&
-                   c.Right >= tile.Left+6 &&
+                   c.Right >= tile.Left + 6 &&
                    c.Top < tile.Bottom &&
                     c.Bottom > tile.Top;
         }
         public static bool IntersectsRight(this Rectangle c, Rectangle tile)
         {
             return c.Right > tile.Right &&
-                //Math.Abs(c.Left) <= Math.Abs(tile.Right)+1  &&
+                   //Math.Abs(c.Left) <= Math.Abs(tile.Right)+1  &&
                    c.Left <= tile.Right &&
-                   c.Top  <  tile.Bottom &&
+                   c.Top < tile.Bottom &&
                    c.Bottom > tile.Top;
         }
-
         public static bool IntersectsTop(this Rectangle c, Rectangle tile)
-        { 
+        {
             return c.Bottom >= tile.Top + 1 &&
-                   c.Bottom <= tile.Top+10 &&
-                   (c.Left   <= tile.Right ||
-                   c.Right  >= tile.Left);
+                   c.Bottom <= tile.Top + 10 &&
+                   (c.Left <= tile.Right ||
+                   c.Right >= tile.Left);
         }
+
+        //more general
+        public static bool IntersectSide(this Rectangle c, Rectangle tile) 
+        {
+            return (c.Left < tile.Right &&
+                       c.Right >= tile.Left)
+                       ||
+                       (c.Right > tile.Left &&
+                       c.Left <= tile.Right);
+        }
+
+        public static bool IntersectY(this Rectangle c, Rectangle tile)
+        {
+            return (c.Top < tile.Bottom &&
+                    c.Bottom >= tile.Top);
+        }
+        
+
+        
          
         
     }
